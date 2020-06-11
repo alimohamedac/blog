86fad9b03331aed456f30983bee492b78930fc34
Ali Mohamed Ali <am6064029@gmail.com>
2020-06-12 00:51:31 +0200

    add theme

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+@include('partial.head') 
+</head>
+
+<body>
+@include('partial.navbar')
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+    @include('partial.content')
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+        @include('partial.footer')
+
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
