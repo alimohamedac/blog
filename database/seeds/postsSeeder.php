6964fe5eaa0ca54a6f0376d3e98d4ca7f674016d
Ali Mohamed Ali <am6064029@gmail.com>
2020-06-13 03:39:20 +0200

    make seeder to add data quickly

---
+++ database/seeds/postsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Illuminate\Support\Facades\DB;
+
+class postsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('posts')->truncate();
+		for($i=0;$i<11;$i++)
+{
+        Post::create([
+        	'title' => 'the '.rand(0,9).' Post',
+        	'body'=> 'This Post is very important because i love alahly ',
+        	'featured'=>'uploads/posts/index.jpg'
+        ]);
+
+
+}
+
+    }
+}
