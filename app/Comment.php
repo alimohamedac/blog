71dd9afe2a6bdd1a8d02c6cd9af3c0bc79b2a61e
Ali Mohamed Ali <am6064029@gmail.com>
2020-06-13 05:22:05 +0200

    make migration & model of Comments

---
+++ app/Comment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Post;
+
+class Comment extends Model
+{
+	protected $fillable = [
+    	'body','post_id'
+    ];
+
+    public function post()
+    {
+    	return $this->belongsTo(Post::Class);
+    }
+}
