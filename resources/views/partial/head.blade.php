d641430fe137520673ae47f934791670775f3727
Ali Mohamed Ali <am6064029@gmail.com>
2020-06-14 14:25:24 +0200

    Make Auth

--- resources/views/partial/head.blade.php
+++ resources/views/partial/head.blade.php
@@ -2,8 +2,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
+   <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>Blog</title>
+  <title>{{ config('app.name','Blog') }}</title>
 
   <!-- Bootstrap core CSS -->
   <link href="{{ asset('themes/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
