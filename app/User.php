62015ac0ad534eec9441594664710b6ce4002386
Ali Mohamed Ali <am6064029@gmail.com>
2020-06-16 01:44:34 +0200

    attach Users & roles

--- app/User.php
+++ app/User.php
@@ -8,6 +8,42 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role','user_role','user_id','role_id');
+    }
+
+    public function hasAnyRole($roles)
+    {
+        if(is_array($roles))
+        {
+            foreach ($roles as $role)
+            {
+                if($this->hasRole($role))
+                {
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            if($this->hasRole($roles))
+                {
+                    return true;
+                }
+        }
+    }
+
+     public function hasRole($role)
+    {
+        if ($this->roles()->where('name',$role)->first())
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
     use Notifiable;
 
     /**
